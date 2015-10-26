7faf2b3ae36080d9799c465eb3542eaecd04ce1c
Wintus <132e84e5@opayq.com>
2015-10-25 23:53:08 -0700

    Define function name

--- PoCsvConverter.py
+++ PoCsvConverter.py
@@ -21,6 +21,24 @@ entry = """
 
 regex_entry = re.compile(entry, re.VERBOSE)
 
+dict_po = dict()  # {text_id:(English, Japanese)}
+
+
+def read_po(po_file_pass=po_file_pass, target_dict=dict_po):
+    pass
+
+
+def read_csv(csv_file_pass=csv_file_pass, target_dict=dict_po):
+    pass
+
+
+def po_to_csv(po_file_pass=po_file_pass, csv_file_pass=csv_file_pass):
+    pass
+
+
+def csv_to_po(csv_file_pass=csv_file_pass, po_file_pass=po_file_pass):
+    pass
+
 
 if __name__ == '__main__':
     pass
