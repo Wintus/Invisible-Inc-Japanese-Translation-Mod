d1aa203bd1f733aa3a8c77012fadc80a391f6989
Wintus <132e84e5@opayq.com>
2015-10-25 23:41:20 -0700

    Define constant

--- PoCsvConverter.py
+++ PoCsvConverter.py
@@ -1,9 +1,26 @@
 # coding: utf-8
 
 import csv
+import re
 
+# constants
 po_file_pass = 'Japanese/japanese.po'
 csv_file_pass = 'japanese.csv'
+
+comment = '#:'
+msgctxt = 'msgctxt'
+msgid = 'msgid'
+msgstr = 'msgstr'
+
+entry = """
+^\#: ([.\w]+)$          # comment. same as msgctxt
+^msgctxt "([.\w]+)"$    # text id
+^msgid "(.*)"$          # original text
+^msgstr "(.*)"$         # translated text
+"""
+
+regex_entry = re.compile(entry, re.VERBOSE)
+
 
 if __name__ == '__main__':
     pass
